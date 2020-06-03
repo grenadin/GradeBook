69e815dc5acbd0e6a17af33810970b243a8e41f4
HARIRAK SRITHAM <harirak@hotmail.co.th>
2020-06-03 20:17:52 +0700

    Add Book Class. in the Book class has Fuction AddGrade(). It use for add number in to the List collection. And has fuction ShowStatistics().It use for calculate grades by lowest grade, highest grade,average grade.

---
+++ GradeBook/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        private List<double> _grades;
+
+        public Book()
+        {
+            _grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            _grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highestGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+            foreach (var number in _grades)
+            {
+                Console.WriteLine($"The number that input into function AddGrade({number})");
+                lowestGrade = Math.Min(number, lowestGrade);
+                highestGrade = Math.Max(number, highestGrade);
+                result += number;
+            }
+            result /= _grades.Count;
+            Console.WriteLine();
+            Console.WriteLine($"The Average grade is {result:N2}");
+            Console.WriteLine($"The Lowest grade is {lowestGrade}");
+            Console.WriteLine($"The Highest grade is {highestGrade}");
+        }
+    }
+}
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -6,7 +6,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var book = new Book();
+            book.AddGrade(95.4);
+            book.AddGrade(45.3);
+            book.AddGrade(21.4);
+            book.AddGrade(99.2);
+            book.ShowStatistics();
         }
     }
 }
